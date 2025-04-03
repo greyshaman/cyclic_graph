d3108f39d11a5e988f0b7b0ba77a2c548df9fb87
Sergey Reshetnikov <shaman@simmirra.com>
2025-04-03 16:55:21 +0300

    improvements for node struct

--- src/cyclic_graph.rs
+++ src/cyclic_graph.rs
@@ -9,21 +9,22 @@ use tokio::sync::RwLock;
 use crate::{error::CyclicGraphError, node::Node};
 
 pub struct CyclicGraph<T> {
-    input: Arc<RwLock<Node<T>>>,
-    output: Arc<RwLock<Node<T>>>,
-    nodes: HashMap<usize, Arc<RwLock<Node<T>>>>,
+    input: Arc<Node<T>>,
+    output: Arc<Node<T>>,
+    nodes: HashMap<usize, Arc<Node<T>>>,
     id_node_counter: AtomicUsize,
 }
 
 impl<T> CyclicGraph<T> {
-    pub fn new(input_data: T, output_data: T) -> Self {
-        let input = Arc::new(RwLock::new(Node::new(0, input_data)));
-
-        let output = Arc::new(RwLock::new(Node::new(1, output_data)));
+    pub async fn new(input_data: T, output_data: T) -> Self {
+        let input = Arc::new(Node::new(0, input_data));
+        let output = Arc::new(Node::new(1, output_data));
 
         let mut nodes = HashMap::new();
         nodes.insert(0, input.clone());
         nodes.insert(1, output.clone());
+
+        input.link_child(output.clone()).await;
 
         Self {
             input,
@@ -36,42 +37,40 @@ impl<T> CyclicGraph<T> {
     pub async fn add_node(
         &mut self,
         data: T,
-        parents: &[usize],
-        children: &[usize],
-    ) -> Result<(), Box<dyn Error>> {
-        if children.iter().any(|id| id == &0) {
+        parent_ids: &[usize],
+        child_ids: &[usize],
+    ) -> Result<Arc<Node<T>>, Box<dyn Error>> {
+        if child_ids.iter().any(|id| id == &0) {
             return Err(Box::new(CyclicGraphError::InsertBeforeInput));
-        } else if parents.iter().any(|id| id == &1) {
+        } else if parent_ids.iter().any(|id| id == &1) {
             return Err(Box::new(CyclicGraphError::InsertAfterOutput));
-        } else {
-            let id = self
-                .id_node_counter
-                .fetch_add(1, std::sync::atomic::Ordering::Release);
-            let new_node = Node::build(id, data);
-            self.nodes.insert(id, new_node.clone());
+        }
 
-            let mut w_new_node = new_node.write().await;
+        let id = self
+            .id_node_counter
+            .fetch_add(1, std::sync::atomic::Ordering::Release);
+        let new_node = Arc::new(Node::new(id, data));
+        self.nodes.insert(id, new_node.clone());
 
-            for parent_id in parents {
-                if let Some(parent) = self.nodes.get(parent_id) {
-                    parent.write().await.add_child(new_node.clone()).await;
-                    w_new_node.add_parent(parent.clone()).await;
-                } else {
-                    return Err(Box::new(CyclicGraphError::NodeNotFoundById(*parent_id)));
-                }
-            }
-
-            for child_id in children {
-                if let Some(child) = self.nodes.get(child_id) {
-                    child.write().await.add_parent(new_node.clone()).await;
-                    w_new_node.add_child(child.clone()).await;
-                } else {
-                    return Err(Box::new(CyclicGraphError::NodeNotFoundById(*child_id)));
-                }
+        for parent_id in parent_ids {
+            if let Some(parent) = self.nodes.get(parent_id) {
+                parent.link_child(new_node.clone()).await;
+                new_node.link_parent(parent.clone()).await;
+            } else {
+                return Err(Box::new(CyclicGraphError::NodeNotFoundById(*parent_id)));
             }
         }
 
-        Ok(())
+        for child_id in child_ids {
+            if let Some(child) = self.nodes.get(child_id) {
+                child.link_parent(new_node.clone()).await;
+                new_node.link_child(child.clone()).await;
+            } else {
+                return Err(Box::new(CyclicGraphError::NodeNotFoundById(*child_id)));
+            }
+        }
+
+        Ok(new_node.clone())
     }
 
     pub async fn insert_between(
@@ -79,44 +78,42 @@ impl<T> CyclicGraph<T> {
         data: T,
         parent_id: usize,
         child_id: usize,
-    ) -> Result<(), Box<dyn Error>> {
+    ) -> Result<Arc<Node<T>>, Box<dyn Error>> {
         if child_id == 0 {
             return Err(Box::new(CyclicGraphError::InsertBeforeInput));
         } else if parent_id == 1 {
             return Err(Box::new(CyclicGraphError::InsertAfterOutput));
-        } else {
-            let parent = self
-                .nodes
-                .get(&parent_id)
-                .ok_or(Box::new(CyclicGraphError::NodeNotFoundById(parent_id)))?
-                .clone();
-            let child = self
-                .nodes
-                .get(&child_id)
-                .ok_or(Box::new(CyclicGraphError::NodeNotFoundById(child_id)))?
-                .clone();
-
-            let id = self
-                .id_node_counter
-                .fetch_add(1, std::sync::atomic::Ordering::Release);
-            let new_node = Node::build(id, data);
-            self.nodes.insert(id, new_node.clone());
-
-            let mut w_parent = parent.write().await;
-            let mut w_child = child.write().await;
-            let mut w_new_node = new_node.write().await;
-            if w_parent.has_child(child_id) {
-                w_parent.remove_child(child_id);
-                w_child.remove_parent(parent_id);
-            }
-            w_parent.add_child(new_node.clone()).await;
-            w_new_node.add_parent(parent.clone()).await;
-
-            w_new_node.add_child(child.clone()).await;
-            w_child.add_parent(new_node.clone()).await;
         }
 
-        Ok(())
+        let parent = self
+            .nodes
+            .get(&parent_id)
+            .ok_or(Box::new(CyclicGraphError::NodeNotFoundById(parent_id)))?
+            .clone();
+        let child = self
+            .nodes
+            .get(&child_id)
+            .ok_or(Box::new(CyclicGraphError::NodeNotFoundById(child_id)))?
+            .clone();
+
+        let id = self
+            .id_node_counter
+            .fetch_add(1, std::sync::atomic::Ordering::Release);
+        let new_node = Arc::new(Node::new(id, data));
+        self.nodes.insert(id, new_node.clone());
+
+        if parent.has_child(child_id).await {
+            parent.unlink_child(child.clone()).await;
+        }
+
+        parent.link_child(new_node.clone()).await;
+        child.link_parent(new_node.clone()).await;
+
+        Ok(new_node.clone())
+    }
+
+    pub fn len(&self) -> usize {
+        self.nodes.len()
     }
 }
 
@@ -126,10 +123,69 @@ mod tests {
 
     #[tokio::test]
     async fn test_new_can_create_cyclic_graph() {
-        let graph = CyclicGraph::new("input_data", "output_data");
+        let graph = CyclicGraph::new("input_data", "output_data").await;
 
-        assert_eq!(graph.input.read().await.id(), 0);
-        assert_eq!(graph.output.read().await.id(), 1);
-        assert_eq!(graph.nodes.len(), 2);
+        assert_eq!(graph.input.id(), 0);
+        assert_eq!(graph.output.id(), 1);
+        assert_eq!(graph.len(), 2);
+
+        assert!(graph.input.has_child(1).await);
+        assert!(graph.output.has_parent(0).await);
+    }
+
+    #[tokio::test]
+    async fn test_add_node_can_add_new_node_to_empty_graph() -> Result<(), Box<dyn Error>> {
+        let mut graph = CyclicGraph::new("input", "output").await;
+        let new_node = graph.add_node("hidden", &[0], &[1]).await?;
+
+        assert_eq!(graph.len(), 3);
+        assert_eq!(new_node.id(), 2);
+        assert!(new_node.has_parent(0).await);
+        assert!(new_node.has_child(1).await);
+
+        assert!(graph.input.has_child(1).await);
+        assert!(graph.input.has_child(2).await);
+        assert!(graph.output.has_parent(0).await);
+        assert!(graph.output.has_parent(2).await);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_add_node_should_return_error_when_input_id_in_children_param() {
+        let mut graph = CyclicGraph::new("input", "output").await;
+        let result = graph.add_node("hidden", &[0], &[0]).await;
+
+        assert!(result.is_err());
+    }
+
+    #[tokio::test]
+    async fn test_add_node_should_return_error_when_output_id_in_parent_param() {
+        let mut graph = CyclicGraph::new("input", "output").await;
+        let result = graph.add_node("hidden", &[1], &[1]).await;
+
+        assert!(result.is_err());
+    }
+
+    #[tokio::test]
+    async fn test_insert_between_create_and_inset_new_node_between_specified_nodes()
+    -> Result<(), Box<dyn Error>> {
+        let mut graph = CyclicGraph::new("input_data", "output_data").await;
+        let result = graph.insert_between("middle", 0, 1).await;
+
+        assert_eq!(graph.len(), 3);
+        assert!(result.is_ok());
+
+        let new_node = result.unwrap();
+        assert_eq!(new_node.id(), 2);
+        assert!(new_node.has_parent(0).await);
+        assert!(new_node.has_child(1).await);
+
+        assert!(!graph.input.has_child(1).await);
+        assert!(graph.input.has_child(2).await);
+        assert!(!graph.output.has_parent(0).await);
+        assert!(graph.output.has_parent(2).await);
+
+        Ok(())
     }
 }
