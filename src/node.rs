b30376c1f9a0cdc38e494bdd196d4d4cb91a4977
Sergey Reshetnikov <shaman@simmirra.com>
2025-04-03 16:54:23 +0300

    improved Node structure

--- src/node.rs
+++ src/node.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::HashMap,
+    collections::{HashMap, HashSet, hash_map::Entry},
     sync::{Arc, Weak},
 };
 
@@ -8,94 +8,67 @@ use tokio::sync::RwLock;
 #[derive(Debug)]
 pub struct Node<T> {
     id: usize,
-    data: T,
-    parents: HashMap<usize, Weak<RwLock<Node<T>>>>,
-    children: HashMap<usize, Arc<RwLock<Node<T>>>>,
+    data: Arc<RwLock<T>>,
+    parent_ids: RwLock<HashSet<usize>>,
+    child_ids: RwLock<HashSet<usize>>,
 }
 
 impl<T> Node<T> {
     pub fn new(id: usize, data: T) -> Self {
         Self {
             id,
-            data,
-            parents: HashMap::new(),
-            children: HashMap::new(),
+            data: Arc::new(RwLock::new(data)),
+            parent_ids: RwLock::new(HashSet::new()),
+            child_ids: RwLock::new(HashSet::new()),
         }
-    }
-
-    pub fn build(id: usize, data: T) -> Arc<RwLock<Node<T>>> {
-        Arc::new(RwLock::new(Node::new(id, data)))
     }
 
     pub fn id(&self) -> usize {
         self.id
     }
 
-    pub fn data(&self) -> &T {
-        &self.data
+    pub fn data(&self) -> Arc<RwLock<T>> {
+        self.data.clone()
     }
 
-    pub fn data_mut(&mut self) -> &mut T {
-        &mut self.data
+    pub async fn set_data(&self, value: T) {
+        *self.data.write().await = value
     }
 
-    pub fn set_data(&mut self, value: T) {
-        self.data = value
+    pub async fn link_child(&self, child: Arc<Node<T>>) -> bool {
+        self.child_ids.write().await.insert(child.id)
+            && child.parent_ids.write().await.insert(self.id)
     }
 
-    pub async fn add_child(&mut self, child: Arc<RwLock<Node<T>>>) {
-        let id = child.read().await.id;
-        if !self.children.contains_key(&id) {
-            self.children.insert(id, Arc::clone(&child));
-        }
+    pub async fn unlink_child(&self, child: Arc<Node<T>>) -> bool {
+        self.child_ids.write().await.remove(&child.id)
+            && child.parent_ids.write().await.remove(&self.id)
     }
 
-    pub fn remove_child(&mut self, id: usize) {
-        self.children.remove(&id);
+    pub async fn has_child(&self, id: usize) -> bool {
+        self.child_ids.read().await.contains(&id)
     }
 
-    pub fn has_child(&self, id: usize) -> bool {
-        self.children.contains_key(&id)
+    pub async fn has_children(&self) -> bool {
+        !self.child_ids.read().await.is_empty()
     }
 
-    pub fn has_children(&self) -> bool {
-        !self.children.is_empty()
+    pub async fn link_parent(&self, parent: Arc<Node<T>>) -> bool {
+        self.parent_ids.write().await.insert(parent.id)
+            && parent.child_ids.write().await.insert(self.id)
     }
 
-    pub fn get_child(&self, id: usize) -> Option<Arc<RwLock<Node<T>>>> {
-        self.children.get(&id).map(|child| child.clone())
+    pub async fn unlink_parent(&self, parent: Arc<Node<T>>) -> bool {
+        self.parent_ids.write().await.remove(&parent.id)
+            && parent.child_ids.write().await.remove(&self.id)
     }
 
-    pub async fn add_parent(&mut self, parent: Arc<RwLock<Node<T>>>) {
-        let id = parent.read().await.id;
-        if !self.parents.contains_key(&id) {
-            self.parents.insert(id, Arc::downgrade(&parent));
-        }
+    pub async fn has_parent(&self, id: usize) -> bool {
+        self.parent_ids.read().await.contains(&id)
     }
 
-    pub fn remove_parent(&mut self, id: usize) {
-        self.parents.remove(&id);
-    }
-
-    pub fn has_parent(&self, id: usize) -> bool {
-        match self.parents.get(&id) {
-            Some(parent_ref) => parent_ref.upgrade().is_some(),
-            None => false,
-        }
-    }
-
-    pub fn has_parents(&self) -> bool {
-        !(self.parents.is_empty())
-            && self
-                .parents
-                .values()
-                .any(|parent| parent.upgrade().is_some())
-    }
-
-    pub fn get_parent(&self, id: usize) -> Option<Arc<RwLock<Node<T>>>> {
-        self.parents
-            .get(&id)
-            .and_then(|parent_ref| parent_ref.upgrade())
+    pub async fn has_parents(&self) -> bool {
+        !self.parent_ids.read().await.is_empty()
     }
 }
 
@@ -103,18 +76,18 @@ impl<T> Node<T> {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_create_new_node() {
+    #[tokio::test]
+    async fn test_create_new_node() {
         let node = Node::new(0, "test");
         let vec_node = Node::new(1, vec![1, 2, 3]);
 
         assert_eq!(node.id, 0);
-        assert_eq!(node.data, "test");
-        assert!(node.parents.is_empty());
-        assert!(node.children.is_empty());
+        assert_eq!(node.data.read().await.clone(), "test");
+        assert!(node.parent_ids.read().await.is_empty());
+        assert!(node.child_ids.read().await.is_empty());
 
         assert_eq!(vec_node.id(), 1);
-        assert_eq!(vec_node.data().len(), 3);
+        assert_eq!(vec_node.data().read().await.len(), 3);
     }
 
     #[test]
@@ -124,101 +97,101 @@ mod tests {
         assert_eq!(node.id(), 0);
     }
 
-    #[test]
-    fn test_data_accessor_should_return_correct_data_ref() {
+    #[tokio::test]
+    async fn test_data_accessor_should_return_correct_data_ref() {
         let node = Node::new(0, "test");
 
-        assert_eq!(node.data, "test");
-    }
-
-    #[test]
-    fn test_data_mut_accessor_allowed_to_change_node_data() {
-        let mut node = Node::new(0, "test");
-
-        let data_mut = node.data_mut();
-        *data_mut = "new test";
-
-        assert_eq!(node.data(), &"new test");
-    }
-
-    #[test]
-    fn test_set_data_should_change_node_data() {
-        let mut node = Node::new(0, "test");
-
-        node.set_data("new test");
-
-        assert_eq!(node.data(), &"new test");
+        assert_eq!(node.data().read().await.clone(), "test");
     }
 
     #[tokio::test]
-    async fn test_linking_two_nodes() {
-        let root_node = Node::build(0, "root");
-        let child_node = Node::build(1, "child");
+    async fn test_data_mut_accessor_allowed_to_change_node_data() {
+        let node = Node::new(0, "test");
 
-        root_node.write().await.add_child(child_node.clone()).await;
+        {
+            let data = node.data();
+            let mut data_mut = data.write().await;
+            *data_mut = "new test";
+        }
 
-        assert!(!root_node.read().await.has_parents());
-        assert!(root_node.read().await.has_children());
-        assert!(!child_node.read().await.has_parents());
-        assert!(!child_node.read().await.has_children());
+        assert_eq!(node.data().read().await.clone(), "new test");
+    }
 
-        child_node.write().await.add_parent(root_node.clone()).await;
+    #[tokio::test]
+    async fn test_set_data_should_change_node_data() {
+        let node = Node::new(0, "test");
 
-        assert!(!root_node.read().await.has_parents());
-        assert!(root_node.read().await.has_children());
-        assert!(child_node.read().await.has_parents());
-        assert!(!child_node.read().await.has_children());
+        node.set_data("new test").await;
+
+        assert_eq!(node.data().read().await.clone(), "new test");
+    }
+
+    #[tokio::test]
+    async fn test_linking_two_nodes_by_link_child() {
+        let root_node = Arc::new(Node::new(0, "root"));
+        let child_node = Arc::new(Node::new(1, "child"));
+
+        assert!(root_node.link_child(child_node.clone()).await);
+
+        assert!(!root_node.has_parents().await);
+        assert!(root_node.has_children().await);
+        assert!(child_node.has_parents().await);
+        assert!(!child_node.has_children().await);
+    }
+
+    #[tokio::test]
+    async fn test_linking_two_nodes_by_link_parent() {
+        let root_node = Arc::new(Node::new(0, "root"));
+        let child_node = Arc::new(Node::new(1, "child"));
+
+        assert!(child_node.link_parent(root_node.clone()).await);
+
+        assert!(!root_node.has_parents().await);
+        assert!(root_node.has_children().await);
+        assert!(child_node.has_parents().await);
+        assert!(!child_node.has_children().await);
     }
 
     #[tokio::test]
     async fn test_has_child_should_return_true_with_correct_child_id() {
-        let node0 = Node::build(0, "root");
-        let node1 = Node::build(1, "child");
+        let node0 = Arc::new(Node::new(0, "root"));
+        let node1 = Arc::new(Node::new(1, "child"));
 
-        node0.write().await.add_child(node1.clone()).await;
+        node0.link_child(node1.clone()).await;
 
-        assert!(node0.read().await.has_child(1));
+        assert!(node0.has_child(1).await);
     }
 
-    #[test]
-    fn test_has_child_should_return_false_with_incorrect_child_id() {
+    #[tokio::test]
+    async fn test_has_child_should_return_false_with_incorrect_child_id() {
         let node = Node::new(0, "test");
 
-        assert!(!node.has_child(1));
+        assert!(!node.has_child(1).await);
     }
 
     #[tokio::test]
     async fn test_has_parent_should_return_true_with_correct_parent_id() {
-        let node0 = Node::build(0, "n0");
-        let node1 = Node::build(1, "n1");
+        let node0 = Arc::new(Node::new(0, "n0"));
+        let node1 = Arc::new(Node::new(1, "n1"));
 
-        node1.write().await.add_parent(node0.clone()).await;
+        node1.link_parent(node0.clone()).await;
 
-        assert!(node1.read().await.has_parent(0));
-    }
-
-    #[test]
-    fn test_has_parent_should_return_false_with_incorrect_parent_id() {
-        let node = Node::new(0, "test");
-
-        assert!(!node.has_parent(1));
+        assert!(node1.has_parent(0).await);
     }
 
     #[tokio::test]
-    async fn test_has_parent_should_return_false_after_parent_dropped() {
-        let child = Node::build(1, "child");
+    async fn test_has_parent_should_return_false_with_incorrect_parent_id() {
+        let node = Node::new(0, "test");
 
-        {
-            let root = Node::build(0, "root");
+        assert!(!node.has_parent(1).await);
+    }
 
-            child.write().await.add_parent(root.clone()).await;
+    #[tokio::test]
+    async fn test_allow_link_self_node_as_child() {
+        let node = Arc::new(Node::new(0, "test"));
 
-            assert!(child.read().await.has_parent(0));
-            // root dropped here
-        }
+        node.link_child(node.clone()).await;
 
-        assert!(child.read().await.get_parent(0).is_none());
-        assert!(!child.read().await.has_parents());
-        assert!(!child.read().await.has_parent(0));
+        assert!(node.has_child(node.id()).await);
     }
 }
