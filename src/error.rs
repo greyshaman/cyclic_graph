deb8662486dd8be7c74aa7fe88343c1ba797cc6a
Sergey Reshetnikov <shaman@simmirra.com>
2025-04-06 16:40:23 +0300

    added  types to error

--- src/error.rs
+++ src/error.rs
@@ -8,6 +8,12 @@ pub enum CyclicGraphError<I> {
     #[error("Cannot insert node after output node")]
     InsertAfterOutput,
 
+    #[error("Cannot remove input node")]
+    RemoveInput,
+
+    #[error("Cannot remove output node")]
+    RemoveOutput,
+
     #[error("Node not found by id: {0}")]
     NodeNotFoundById(I),
 
