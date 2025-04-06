b70ca1b04f4b6ab9bbfeac53d68c08aa8cf940dd
Sergey Reshetnikov <shaman@simmirra.com>
2025-04-06 06:24:27 +0300

    added generic types to has_child and has_parent. Added tests unit for Nodes with string id

--- src/node.rs
+++ src/node.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, hash::Hash, sync::Arc};
+use std::{borrow::Borrow, collections::HashSet, hash::Hash, sync::Arc};
 
 use tokio::sync::RwLock;
 
@@ -45,7 +45,11 @@ where
             && child.parent_ids.write().await.remove(self.id())
     }
 
-    pub async fn has_child(&self, id: &I) -> bool {
+    pub async fn has_child<Q: ?Sized>(&self, id: &Q) -> bool
+    where
+        I: Borrow<Q>,
+        Q: Hash + Eq,
+    {
         self.child_ids.read().await.contains(id)
     }
 
@@ -72,7 +76,11 @@ where
             && parent.child_ids.write().await.remove(self.id())
     }
 
-    pub async fn has_parent(&self, id: &I) -> bool {
+    pub async fn has_parent<Q: ?Sized>(&self, id: &Q) -> bool
+    where
+        I: Borrow<Q>,
+        Q: Hash + Eq,
+    {
         self.parent_ids.read().await.contains(id)
     }
 
@@ -99,35 +107,35 @@ mod tests {
 
         #[tokio::test]
         async fn test_create_new_node() {
-            let node = Node::new(0, "test");
-            let vec_node = Node::new(1, vec![1, 2, 3]);
+            let node = Node::new(0_usize, "test");
+            let vec_node = Node::new(1_usize, vec![1_usize, 2, 3]);
 
             assert_eq!(node.id, 0);
             assert_eq!(node.data.read().await.clone(), "test");
             assert!(node.parent_ids.read().await.is_empty());
             assert!(node.child_ids.read().await.is_empty());
 
-            assert_eq!(vec_node.id(), &1);
+            assert_eq!(vec_node.id, 1);
             assert_eq!(vec_node.data().read().await.len(), 3);
         }
 
         #[test]
         fn test_id_accessor_should_return_correct_id_value() {
-            let node = Node::new(0, "test");
+            let node = Node::new(0_usize, "test");
 
             assert_eq!(node.id(), &0);
         }
 
         #[tokio::test]
         async fn test_data_accessor_should_return_correct_data_ref() {
-            let node = Node::new(0, "test");
+            let node = Node::new(0_usize, "test");
 
             assert_eq!(node.data().read().await.clone(), "test");
         }
 
         #[tokio::test]
         async fn test_data_mut_accessor_allowed_to_change_node_data() {
-            let node = Node::new(0, "test");
+            let node = Node::new(0_usize, "test");
 
             {
                 let data = node.data();
@@ -140,7 +148,7 @@ mod tests {
 
         #[tokio::test]
         async fn test_set_data_should_change_node_data() {
-            let node = Node::new(0, "test");
+            let node = Node::new(0_usize, "test");
 
             node.set_data("new test").await;
 
@@ -149,8 +157,8 @@ mod tests {
 
         #[tokio::test]
         async fn test_linking_two_nodes_by_link_child() {
-            let root_node = Arc::new(Node::new(0, "root"));
-            let child_node = Arc::new(Node::new(1, "child"));
+            let root_node = Arc::new(Node::new(0_usize, "root"));
+            let child_node = Arc::new(Node::new(1_usize, "child"));
 
             assert!(root_node.link_child(child_node.clone()).await);
 
@@ -162,8 +170,8 @@ mod tests {
 
         #[tokio::test]
         async fn test_second_link_child_attempt_should_return_false() {
-            let parent = Arc::new(Node::new(0, "parent"));
-            let child = Arc::new(Node::new(1, "child"));
+            let parent = Arc::new(Node::new(0_usize, "parent"));
+            let child = Arc::new(Node::new(1_usize, "child"));
 
             assert!(parent.link_child(child.clone()).await);
             assert!(!parent.link_child(child.clone()).await);
@@ -171,8 +179,8 @@ mod tests {
 
         #[tokio::test]
         async fn test_second_link_parent_attempt_should_return_false() {
-            let parent = Arc::new(Node::new(0, "parent"));
-            let child = Arc::new(Node::new(1, "child"));
+            let parent = Arc::new(Node::new(0_usize, "parent"));
+            let child = Arc::new(Node::new(1_usize, "child"));
 
             assert!(child.link_parent(parent.clone()).await);
             assert!(!child.link_parent(parent.clone()).await);
@@ -180,8 +188,8 @@ mod tests {
 
         #[tokio::test]
         async fn test_unlink_child_should_break_link_between_linked_nodes() {
-            let parent = Arc::new(Node::new(0, "parent"));
-            let child = Arc::new(Node::new(1, "child"));
+            let parent = Arc::new(Node::new(0_usize, "parent"));
+            let child = Arc::new(Node::new(1_usize, "child"));
 
             assert!(parent.link_child(child.clone()).await);
 
@@ -193,8 +201,8 @@ mod tests {
 
         #[tokio::test]
         async fn test_second_unlink_child_should_return_false() {
-            let parent = Arc::new(Node::new(0, "parent"));
-            let child = Arc::new(Node::new(1, "child"));
+            let parent = Arc::new(Node::new(0_usize, "parent"));
+            let child = Arc::new(Node::new(1_usize, "child"));
 
             assert!(parent.link_child(child.clone()).await);
 
@@ -204,8 +212,8 @@ mod tests {
 
         #[tokio::test]
         async fn test_unlink_parent_should_break_link_between_linked_nodes() {
-            let parent = Arc::new(Node::new(0, "parent"));
-            let child = Arc::new(Node::new(1, "child"));
+            let parent = Arc::new(Node::new(0_usize, "parent"));
+            let child = Arc::new(Node::new(1_usize, "child"));
 
             assert!(parent.link_child(child.clone()).await);
 
@@ -217,8 +225,8 @@ mod tests {
 
         #[tokio::test]
         async fn test_second_unlink_parent_should_return_false() {
-            let parent = Arc::new(Node::new(0, "parent"));
-            let child = Arc::new(Node::new(1, "child"));
+            let parent = Arc::new(Node::new(0_usize, "parent"));
+            let child = Arc::new(Node::new(1_usize, "child"));
 
             assert!(child.link_parent(parent.clone()).await);
 
@@ -228,10 +236,10 @@ mod tests {
 
         #[tokio::test]
         async fn test_child_ids_should_return_children_identifiers_in_vector() {
-            let parent = Arc::new(Node::new(0, "parent"));
+            let parent = Arc::new(Node::new(0_usize, "parent"));
 
-            let child1 = Arc::new(Node::new(1, "child1"));
-            let child2 = Arc::new(Node::new(2, "child2"));
+            let child1 = Arc::new(Node::new(1_usize, "child1"));
+            let child2 = Arc::new(Node::new(2_usize, "child2"));
 
             assert!(parent.link_child(child1.clone()).await);
             assert!(parent.link_child(child2.clone()).await);
@@ -246,10 +254,10 @@ mod tests {
 
         #[tokio::test]
         async fn test_children_ids_should_return_children_identifiers_in_vector() {
-            let parent1 = Arc::new(Node::new(0, "parent"));
-            let parent2 = Arc::new(Node::new(1, "parent"));
+            let parent1 = Arc::new(Node::new(0_usize, "parent"));
+            let parent2 = Arc::new(Node::new(1_usize, "parent"));
 
-            let child = Arc::new(Node::new(2, "child1"));
+            let child = Arc::new(Node::new(2_usize, "child1"));
 
             assert!(parent1.link_child(child.clone()).await);
             assert!(parent2.link_child(child.clone()).await);
@@ -264,8 +272,8 @@ mod tests {
 
         #[tokio::test]
         async fn test_linking_two_nodes_by_link_parent() {
-            let root_node = Arc::new(Node::new(0, "root"));
-            let child_node = Arc::new(Node::new(1, "child"));
+            let root_node = Arc::new(Node::new(0_usize, "root"));
+            let child_node = Arc::new(Node::new(1_usize, "child"));
 
             assert!(child_node.link_parent(root_node.clone()).await);
 
@@ -277,8 +285,8 @@ mod tests {
 
         #[tokio::test]
         async fn test_has_child_should_return_true_with_correct_child_id() {
-            let node0 = Arc::new(Node::new(0, "root"));
-            let node1 = Arc::new(Node::new(1, "child"));
+            let node0 = Arc::new(Node::new(0_usize, "root"));
+            let node1 = Arc::new(Node::new(1_usize, "child"));
 
             node0.link_child(node1.clone()).await;
 
@@ -287,15 +295,15 @@ mod tests {
 
         #[tokio::test]
         async fn test_has_child_should_return_false_with_incorrect_child_id() {
-            let node = Node::new(0, "test");
+            let node = Node::new(0_usize, "test");
 
             assert!(!node.has_child(&1).await);
         }
 
         #[tokio::test]
         async fn test_has_parent_should_return_true_with_correct_parent_id() {
-            let node0 = Arc::new(Node::new(0, "n0"));
-            let node1 = Arc::new(Node::new(1, "n1"));
+            let node0 = Arc::new(Node::new(0_usize, "n0"));
+            let node1 = Arc::new(Node::new(1_usize, "n1"));
 
             node1.link_parent(node0.clone()).await;
 
@@ -304,14 +312,239 @@ mod tests {
 
         #[tokio::test]
         async fn test_has_parent_should_return_false_with_incorrect_parent_id() {
-            let node = Node::new(0, "test");
+            let node = Node::new(0_usize, "test");
 
             assert!(!node.has_parent(&1).await);
         }
 
         #[tokio::test]
         async fn test_allow_link_self_node_as_child() {
-            let node = Arc::new(Node::new(0, "test"));
+            let node = Arc::new(Node::new(0_usize, "test"));
+
+            node.link_child(node.clone()).await;
+
+            assert!(node.has_child(node.id()).await);
+        }
+    }
+
+    mod for_id_as_str {
+        use super::*;
+
+        #[tokio::test]
+        async fn test_create_new_node() {
+            let node = Node::new("IL", "test");
+            let vec_node = Node::new("IL", vec![1, 2, 3]);
+
+            assert_eq!(node.id, "IL");
+            assert_eq!(node.data.read().await.clone(), "test");
+            assert!(node.parent_ids.read().await.is_empty());
+            assert!(node.child_ids.read().await.is_empty());
+
+            assert_eq!(vec_node.id, "IL");
+            assert_eq!(vec_node.data().read().await.len(), 3);
+        }
+
+        #[test]
+        fn test_id_accessor_should_return_correct_id_value() {
+            let node = Node::new("IL", "test");
+
+            assert_eq!(node.id(), &"IL");
+        }
+
+        #[tokio::test]
+        async fn test_data_accessor_should_return_correct_data_ref() {
+            let node = Node::new("IL", "test");
+
+            assert_eq!(node.data().read().await.clone(), "test");
+        }
+
+        #[tokio::test]
+        async fn test_data_mut_accessor_allowed_to_change_node_data() {
+            let node = Node::new("IL", "test");
+
+            {
+                let data = node.data();
+                let mut data_mut = data.write().await;
+                *data_mut = "new test";
+            }
+
+            assert_eq!(node.data().read().await.clone(), "new test");
+        }
+
+        #[tokio::test]
+        async fn test_set_data_should_change_node_data() {
+            let node = Node::new("IL", "test");
+
+            node.set_data("new test").await;
+
+            assert_eq!(node.data().read().await.clone(), "new test");
+        }
+
+        #[tokio::test]
+        async fn test_linking_two_nodes_by_link_child() {
+            let root_node = Arc::new(Node::new("n0", "root"));
+            let child_node = Arc::new(Node::new("n1", "child"));
+
+            assert!(root_node.link_child(child_node.clone()).await);
+
+            assert!(!root_node.has_parents().await);
+            assert!(root_node.has_children().await);
+            assert!(child_node.has_parents().await);
+            assert!(!child_node.has_children().await);
+        }
+
+        #[tokio::test]
+        async fn test_second_link_child_attempt_should_return_false() {
+            let parent = Arc::new(Node::new("n0", "parent"));
+            let child = Arc::new(Node::new("n1", "child"));
+
+            assert!(parent.link_child(child.clone()).await);
+            assert!(!parent.link_child(child.clone()).await);
+        }
+
+        #[tokio::test]
+        async fn test_second_link_parent_attempt_should_return_false() {
+            let parent = Arc::new(Node::new("n0", "parent"));
+            let child = Arc::new(Node::new("n1", "child"));
+
+            assert!(child.link_parent(parent.clone()).await);
+            assert!(!child.link_parent(parent.clone()).await);
+        }
+
+        #[tokio::test]
+        async fn test_unlink_child_should_break_link_between_linked_nodes() {
+            let parent = Arc::new(Node::new("n0", "parent"));
+            let child = Arc::new(Node::new("n1", "child"));
+
+            assert!(parent.link_child(child.clone()).await);
+
+            assert!(parent.unlink_child(child.clone()).await);
+
+            assert!(!parent.has_children().await);
+            assert!(!child.has_parents().await);
+        }
+
+        #[tokio::test]
+        async fn test_second_unlink_child_should_return_false() {
+            let parent = Arc::new(Node::new("n0", "parent"));
+            let child = Arc::new(Node::new("n1", "child"));
+
+            assert!(parent.link_child(child.clone()).await);
+
+            assert!(parent.unlink_child(child.clone()).await);
+            assert!(!parent.unlink_child(child.clone()).await);
+        }
+
+        #[tokio::test]
+        async fn test_unlink_parent_should_break_link_between_linked_nodes() {
+            let parent = Arc::new(Node::new("n0", "parent"));
+            let child = Arc::new(Node::new("n1", "child"));
+
+            assert!(parent.link_child(child.clone()).await);
+
+            assert!(child.unlink_parent(parent.clone()).await);
+
+            assert!(!parent.has_children().await);
+            assert!(!child.has_parents().await);
+        }
+
+        #[tokio::test]
+        async fn test_second_unlink_parent_should_return_false() {
+            let parent = Arc::new(Node::new("n0", "parent"));
+            let child = Arc::new(Node::new("n1", "child"));
+
+            assert!(child.link_parent(parent.clone()).await);
+
+            assert!(child.unlink_parent(parent.clone()).await);
+            assert!(!child.unlink_parent(parent.clone()).await);
+        }
+
+        #[tokio::test]
+        async fn test_child_ids_should_return_children_identifiers_in_vector() {
+            let parent = Arc::new(Node::new("n0", "parent"));
+
+            let child1 = Arc::new(Node::new("n1", "child1"));
+            let child2 = Arc::new(Node::new("n2", "child2"));
+
+            assert!(parent.link_child(child1.clone()).await);
+            assert!(parent.link_child(child2.clone()).await);
+
+            let ids = parent.child_ids().await;
+
+            assert!(!ids.is_empty());
+            assert_eq!(ids.len(), 2);
+            assert!(ids.contains(&"n1"));
+            assert!(ids.contains(&"n2"));
+        }
+
+        #[tokio::test]
+        async fn test_children_ids_should_return_children_identifiers_in_vector() {
+            let parent1 = Arc::new(Node::new("n0", "parent"));
+            let parent2 = Arc::new(Node::new("n1", "parent"));
+
+            let child = Arc::new(Node::new("n2", "child1"));
+
+            assert!(parent1.link_child(child.clone()).await);
+            assert!(parent2.link_child(child.clone()).await);
+
+            let ids = child.parent_ids().await;
+
+            assert!(!ids.is_empty());
+            assert_eq!(ids.len(), 2);
+            assert!(ids.contains(&"n0"));
+            assert!(ids.contains(&"n1"));
+        }
+
+        #[tokio::test]
+        async fn test_linking_two_nodes_by_link_parent() {
+            let root_node = Arc::new(Node::new("n0", "root"));
+            let child_node = Arc::new(Node::new("n1", "child"));
+
+            assert!(child_node.link_parent(root_node.clone()).await);
+
+            assert!(!root_node.has_parents().await);
+            assert!(root_node.has_children().await);
+            assert!(child_node.has_parents().await);
+            assert!(!child_node.has_children().await);
+        }
+
+        #[tokio::test]
+        async fn test_has_child_should_return_true_with_correct_child_id() {
+            let node0 = Arc::new(Node::new("n0", "root"));
+            let node1 = Arc::new(Node::new("n1", "child"));
+
+            node0.link_child(node1.clone()).await;
+
+            assert!(node0.has_child(&"n1").await);
+        }
+
+        #[tokio::test]
+        async fn test_has_child_should_return_false_with_incorrect_child_id() {
+            let node = Node::new("n0", "test");
+
+            assert!(!node.has_child(&"n1").await);
+        }
+
+        #[tokio::test]
+        async fn test_has_parent_should_return_true_with_correct_parent_id() {
+            let node0 = Arc::new(Node::new("n0", "n0"));
+            let node1 = Arc::new(Node::new("n1", "n1"));
+
+            node1.link_parent(node0.clone()).await;
+
+            assert!(node1.has_parent(&"n0").await);
+        }
+
+        #[tokio::test]
+        async fn test_has_parent_should_return_false_with_incorrect_parent_id() {
+            let node = Node::new("n0", "test");
+
+            assert!(!node.has_parent(&"n1").await);
+        }
+
+        #[tokio::test]
+        async fn test_allow_link_self_node_as_child() {
+            let node = Arc::new(Node::new("n0", "test"));
 
             node.link_child(node.clone()).await;
 
