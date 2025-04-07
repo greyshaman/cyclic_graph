4aee737adf1713e3980ef3ab4938c2f29b394926
Sergey Reshetnikov <shaman@simmirra.com>
2025-04-07 19:37:21 +0300

    Added code documentation

--- src/error.rs
+++ src/error.rs
@@ -1,22 +1,29 @@
 use thiserror::Error;
 
+/// Project specific error types
 #[derive(Error, Debug)]
 pub enum CyclicGraphError<I> {
+    /// The error was caused when new node inserting before the Input Node
     #[error("Cannot insert node before input node")]
     InsertBeforeInput,
 
+    /// The error was caused when new node appending after the Output Node
     #[error("Cannot insert node after output node")]
     InsertAfterOutput,
 
+    /// The error was caused when trying to remove the Input Node
     #[error("Cannot remove input node")]
     RemoveInput,
 
+    /// The error was caused when trying to remove the Output Node
     #[error("Cannot remove output node")]
     RemoveOutput,
 
+    /// The error was caused when trying get non-existing node by id
     #[error("Node not found by id: {0}")]
     NodeNotFoundById(I),
 
+    /// The error wes caused when trying add node with non-unique id
     #[error("Entered id `{0}` is not unique")]
     NonUniqueId(I),
 }
