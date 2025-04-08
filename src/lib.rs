423a874c3414e4637afa699716098abe6f30373a
Sergey Reshetnikov <shaman@simmirra.com>
2025-04-08 21:21:53 +0300

    moved GeneratorMode into id_generator module

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod cyclic_graph;
 pub mod error;
 pub mod node;
+pub mod id_generator;
 
-pub use cyclic_graph::{CyclicGraph, GeneratorMode};
+pub use cyclic_graph::CyclicGraph;
 pub use error::CyclicGraphError as Error;
 pub use node::Node;
+pub use id_generator::{IdGenerator, GeneratorMode};
