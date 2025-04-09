5749aef157f8fbf8a52e630f9d72171705615b8a
Sergey Reshetnikov <shaman@simmirra.com>
2025-04-09 11:32:40 +0300

    fixed code style

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 pub mod cyclic_graph;
 pub mod error;
-pub mod node;
 pub mod id_generator;
+pub mod node;
 
 pub use cyclic_graph::CyclicGraph;
 pub use error::CyclicGraphError as Error;
+pub use id_generator::{GeneratorMode, IdGenerator};
 pub use node::Node;
-pub use id_generator::{IdGenerator, GeneratorMode};
