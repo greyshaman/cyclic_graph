68e95e48a82d104267c19d2857d10d5121a73351
Sergey Reshetnikov <shaman@simmirra.com>
2025-05-17 14:58:49 +0300

    Change imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
-pub mod cyclic_graph;
-pub mod default_handler;
+// pub mod cyclic_graph;
+pub mod content;
 pub mod error;
 pub mod id_generator;
-pub mod links_acceptor;
-pub mod links_provider;
 pub mod node;
 
-pub use cyclic_graph::CyclicGraph;
+// pub use cyclic_graph::CyclicGraph;
+pub use content::Content;
 pub use error::CyclicGraphError as Error;
 pub use id_generator::{GeneratorMode, IdGenerator};
 pub use node::Node;
