97a6c2eddf9e671aca847dca36742f9450f644c2
Sergey Reshetnikov <shaman@simmirra.com>
2025-05-20 07:18:00 +0300

    Added method for downcast dyn trait  objects

--- src/content.rs
+++ src/content.rs
@@ -1,27 +1,37 @@
-use std::{fmt::Debug, sync::Arc};
+use std::{any::Any, fmt::Debug, sync::Arc};
 
 use crate::Error as CGError;
 use async_trait::async_trait;
 use tokio::sync::{RwLock, broadcast};
 
 #[async_trait]
-pub trait Content<I: 'static, D: 'static, S: 'static>: Debug + Send + Sync {
-    async fn data(&self) -> Arc<RwLock<D>>;
+pub trait Content<I: 'static, D: 'static, S: 'static>: Debug + Send + Sync + Any {
+    fn as_any(&self) -> &dyn Any;
 
-    async fn set_data(&mut self, data: Arc<RwLock<D>>) -> Result<Arc<RwLock<D>>, CGError<I>>;
+    fn data(&self) -> Arc<RwLock<D>>;
+
+    fn set_data(&mut self, data: Arc<RwLock<D>>) -> Result<Arc<RwLock<D>>, CGError<I>>;
 
     async fn provide_receiver(
-        &mut self,
+        &self,
         _src_idx: usize,
     ) -> Result<Option<broadcast::Receiver<S>>, CGError<I>> {
         Ok(None)
     }
 
     fn try_provide_receiver(
-        &mut self,
+        &self,
         _src_idx: usize,
     ) -> Result<Option<broadcast::Receiver<S>>, CGError<I>> {
         Ok(None)
+    }
+
+    async fn provide_src_ids(&self) -> Vec<usize> {
+        vec![]
+    }
+
+    fn try_provide_src_ids(&self) -> Result<Vec<usize>, CGError<I>> {
+        Ok(vec![])
     }
 
     async fn link_accept(
