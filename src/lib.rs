4cabe53fd4587a6cfbd8e89d98e512fbc063cbfd
Sergey Reshetnikov <shaman@simmirra.com>
2025-05-21 08:07:41 +0300

    enable cyclic_graph import

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-// pub mod cyclic_graph;
 pub mod content;
+pub mod cyclic_graph;
 pub mod error;
 pub mod id_generator;
 pub mod node;
 
-// pub use cyclic_graph::CyclicGraph;
 pub use content::Content;
+pub use cyclic_graph::CyclicGraph;
 pub use error::CyclicGraphError as Error;
 pub use id_generator::{GeneratorMode, IdGenerator};
 pub use node::Node;
